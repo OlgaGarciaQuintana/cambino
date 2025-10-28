413c7a4d65623f5cd12df42bbb11e50e4863e011
Olga Garcia <olgarciaq11@gmail.com>
2025-10-28 10:09:13 +0100

    falta arreglar que te salude

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home', //esto es para usarlo en html
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
   standalone: true
