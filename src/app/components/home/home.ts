821715f51d991dccd4b08f88a7e9c63fc124611e
Olga Garcia <olgarciaq11@gmail.com>
2025-10-28 09:32:59 +0100

    correcion y comentarios

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -11,50 +11,12 @@ import { RouterLink } from "@angular/router";
 })
 export class Home {
 
-  nombre = "Olga";
-  titulo = "Deku"
-  ancho=300;
-  hasError = false;
-  mostrar = false;
-  items= ["Manzana", "Banana", "Naranja"];
-  today = new Date();
-
   constructor(){
-    console.log("constructor");
-    this.nombre="Jose";
+   
   }
 
   ngOnInit(){
-    console.log("ngOnInit")
-    this.nombre="Ana";
-  }
-
-  nohayError() {
-    return this.hasError;
-  }
-
-  sihayError() {
-    return !this.hasError;
-  }
-
-  suma(a: number, b: number): number {
-    return a + b;
-  }
-
-  cambiarNombre() {
-    this.nombre = "Juan";
-  }
-
-  cambiarNombreInput(event: any) {
-    this.nombre = event.target.value;
-  }
-
-  flipMostrar() {
-    this.mostrar = !this.mostrar;
-  }
-
-  complicado() {
-    return [1,2,3].map(n => n * 2).filter(n => n > 2);
+   
   }
 
 }
