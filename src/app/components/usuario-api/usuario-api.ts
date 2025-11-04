d9b1624e3ea5df8a7d9eb2c8b01ab382ecc1e4c5
Olga <olgarciaq11@gmail.com>
2025-11-04 21:20:20 +0100

    Añadir api

--- src/app/components/usuario-api/usuario-api.ts
+++ src/app/components/usuario-api/usuario-api.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Character, CharacterResponse } from '../../model/apiInterface';
 
 @Component({
   selector: 'app-usuario-api',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './usuario-api.html',
   styleUrl: './usuario-api.css',
 })
-export class UsuarioAPI {
+export class UsuarioAPI implements OnInit {
+  personajes: Character[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getCharacters();
+  }
+
+  getCharacters() {
+    const url = 'https://corsproxy.io/?' + encodeURIComponent('https://www.demonslayer-api.com/api/v1/characters');
+    
+    this.http.get<CharacterResponse>(url).subscribe({
+      next: (response: CharacterResponse) => {
+        console.log(response);
+        this.personajes = response.content;
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      }
+    });
+  }
 }
