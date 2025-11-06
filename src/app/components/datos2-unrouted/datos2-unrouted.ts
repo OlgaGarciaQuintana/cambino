a13e7aeb1e66fd0002a2e486e88106deac2dba3e
Olga Garcia <olgarciaq11@gmail.com>
2025-11-06 08:14:42 +0100

    arreglado

--- src/app/components/datos2-unrouted/datos2-unrouted.ts
+++ src/app/components/datos2-unrouted/datos2-unrouted.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
-import { User } from '../../model/userInterface';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { User } from '../../model/userInterface';
 import { JsonplaceholderService } from '../../services/jsonplaceholder-service';
 
 @Component({
@@ -10,17 +10,30 @@ import { JsonplaceholderService } from '../../services/jsonplaceholder-service';
   styleUrl: './datos2-unrouted.css',
 })
 export class Datos2Unrouted {
- data = inject(MAT_DIALOG_DATA);
-  nUsuario:number = 0;
+  data = inject(MAT_DIALOG_DATA);
+  nUsuario: number = 0;
+  oUsuario: User | null = null;
+  loading: boolean = true;
 
-  constructor(private oJsonplaceholderService: JsonplaceholderService) {}
+  constructor(private oJsonplaceholderService: JsonplaceholderService) { 
+    console.log("Datos recibidos en el hijo en el diálogo:", this.data);
+    this.nUsuario = this.data.usuario_id;
+    this.oJsonplaceholderService.getUser(this.nUsuario).subscribe({
+      next: (user: User) => {
+        console.log("Usuario obtenido:", user);
+        this.oUsuario = user;
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error("Error al obtener el usuario:", error);
+        this.loading = false;
+      }
+    });
+
+  }
 
   ngOnInit() {
-    console.log('Datos recibidos en el hijo en el diálogo:', this.data);
-    this.nUsuario=this.data.usuario_id;
-    this.oJsonplaceholderService.getUser(this.nUsuario).subscribe((user: User[]) => {
-      console.log("Usuario obtenido:", user);
-      this.oUsuario = user;
-      }
+
   }
+
 }
